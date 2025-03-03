17a4c0f7585f786f8e07d5e4675bc45d981170b5
FelpisLipe <176348835+FelpisLipe@users.noreply.github.com>
2025-03-03 03:33:04 -0300

    Initial Commit (and first item)

---
+++ src/main/java/com/felpslipe/simple_utils/item/SimpleItems.java
@@ -0,0 +1,19 @@
+package com.felpslipe.simple_utils.item;
+
+import com.felpslipe.simple_utils.SimpleUtils;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class SimpleItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(SimpleUtils.MOD_ID);
+
+    public static final DeferredItem<Item> CRIMSON_DUST = ITEMS.register("crimson_dust",
+            () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
